1daa55f96d55d0086f56ae39dbfc9d7da108369a
v_lvhang02 <v_lvhang02@baidu.com>
2025-06-17 22:07:05 +0800

    hot100

---
+++ leetcode100/136_single_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    class Solution {
+        public:
+            int singleNumber(vector<int>& nums){
+                int result = 0;
+                for(int num : nums){
+                    result ^= num;
+                }
+                return result;
+            }
+    };
+}
+
